d0f2b4ebedf3a02bfaa797b9f1e2d8585e540457
mo3310 <str.ftg0790@gmail.com>
2021-07-26 07:10:46 +0000

    いいね機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   has_many :tag_maps, dependent: :destroy
   has_many :tags, through: :tag_maps
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   def save_tag(sent_tags)
     current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   root 'posts#index'
   resources :posts, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 
