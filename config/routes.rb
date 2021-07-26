e82e9aadeae57f21dbc4f2ed2c3c50937e593e16
mo3310 <str.ftg0790@gmail.com>
2021-07-26 08:28:02 +0000

    マイページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'homes#top'
-  root 'posts#index'
   resources :posts, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 
   #タグによって絞り込んだ投稿を表示するアクションへのルーティング
   resources :tags do
