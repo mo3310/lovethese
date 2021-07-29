44afa16174ef963ca0ddc3eadacd05f57963ea44
mo3310 <str.ftg0790@gmail.com>
2021-07-29 13:12:20 +0000

    [update]ゲストログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,36 @@
 class UsersController < ApplicationController
+  before_action :ensure_normal_user, only: [:update, :edit]
+
+  def ensure_normal_user
+    if current_user.email == 'guest@example.com'
+      redirect_to root_path
+      flash[:success] = 'ゲストユーザーの編集・削除はできません。'
+    end
+  end
 
   def show
     @user = User.find(params[:id])
+    @post = Post.new
     @posts = @user.posts.page(params[:page]).reverse_order
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:success] = "プロフィールを更新しました。"
+    else
+      render "edit"
+    end
   end
 
   private
@@ -20,6 +38,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-
+  def user_params_update
+    params.require(:user).permit(:name, :email, :profile_image)
+  end
 
 end
